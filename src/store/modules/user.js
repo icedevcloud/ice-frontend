cf0e44f8cfe6284706b842653170b14cf7a4e694
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-10-21 00:29:15 +0800

    feat:登录获取用户信息

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { login, getInfo, logout } from '@/api/login'
+import { authToken, getInfo, getUserInfo, logout } from '@/api/login'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { welcome } from '@/utils/util'
 
@@ -36,9 +36,9 @@ const user = {
     // 登录
     Login ({ commit }, userInfo) {
       return new Promise((resolve, reject) => {
-        login(userInfo).then(response => {
-          const result = response.result
-          Vue.ls.set(ACCESS_TOKEN, result.token, 7 * 24 * 60 * 60 * 1000)
+        authToken(userInfo).then(response => {
+          const result = response.data
+          Vue.ls.set(ACCESS_TOKEN, result.access_token, 7 * 24 * 60 * 60 * 1000)
           commit('SET_TOKEN', result.token)
           resolve()
         }).catch(error => {
@@ -50,26 +50,13 @@ const user = {
     // 获取用户信息
     GetInfo ({ commit }) {
       return new Promise((resolve, reject) => {
-        getInfo().then(response => {
-          const result = response.result
+        getUserInfo().then(response => {
+          const result = response.data
 
-          if (result.role && result.role.permissions.length > 0) {
-            const role = result.role
-            role.permissions = result.role.permissions
-            role.permissions.map(per => {
-              if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
-                const action = per.actionEntitySet.map(action => { return action.action })
-                per.actionList = action
-              }
-            })
-            role.permissionList = role.permissions.map(permission => { return permission.permissionId })
-            commit('SET_ROLES', result.role)
-            commit('SET_INFO', result)
-          } else {
-            reject(new Error('getInfo: roles must be a non-null array !'))
-          }
+          commit('SET_ROLES', {})
+          commit('SET_INFO', result)
 
-          commit('SET_NAME', { name: result.name, welcome: welcome() })
+          commit('SET_NAME', { name: result.nickname, welcome: welcome() })
           commit('SET_AVATAR', result.avatar)
 
           resolve(response)
@@ -77,6 +64,35 @@ const user = {
           reject(error)
         })
       })
+      // return new Promise((resolve, reject) => {
+      //   getInfo().then(response => {
+      //     const result = response.result
+      //     console.log('user', result)
+      //
+      //     if (result.role && result.role.permissions.length > 0) {
+      //       const role = result.role
+      //       role.permissions = result.role.permissions
+      //       role.permissions.map(per => {
+      //         if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
+      //           const action = per.actionEntitySet.map(action => { return action.action })
+      //           per.actionList = action
+      //         }
+      //       })
+      //       role.permissionList = role.permissions.map(permission => { return permission.permissionId })
+      //       commit('SET_ROLES', result.role)
+      //       commit('SET_INFO', result)
+      //     } else {
+      //       reject(new Error('getInfo: roles must be a non-null array !'))
+      //     }
+      //
+      //     commit('SET_NAME', { name: result.name, welcome: welcome() })
+      //     commit('SET_AVATAR', result.avatar)
+      //
+      //     resolve(response)
+      //   }).catch(error => {
+      //     reject(error)
+      //   })
+      // })
     },
 
     // 登出
